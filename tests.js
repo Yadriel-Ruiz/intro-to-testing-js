3ba975bc273b4878a95403eb9cc2770172c3dbcc
Yadriel-Ruiz <147550067+Yadriel-Ruiz@users.noreply.github.com>
2023-10-18 15:13:50 -0500

    add the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -13,3 +13,9 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function(){
+    it('should is a defined function',function () {
+        expect(typeof sayHello).toBe('function');
+    });
+})
