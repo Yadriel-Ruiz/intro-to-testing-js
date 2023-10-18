d36c2e9ecc77fcd841e87c879903911cde1c2806
Yadriel-Ruiz <147550067+Yadriel-Ruiz@users.noreply.github.com>
2023-10-18 16:52:00 -0500

    unit tests for edge cases.

--- tests.js
+++ tests.js
@@ -22,14 +22,21 @@ describe('sayHello', function(){
         expect(typeof sayHello()).toBe("string");
     });
     it('should return the string "Hello, Jane!', function() {
-        expect(sayHello()).toBe("Hello, Jane!");
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
     it('should sayHello("Alex") return "Hello, Alex!', function (){
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
     it('should sayHello("Pat")return "Hello, Pat!)', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
-        
     });
-
+    it('should sayHello("World") return "Hello, World!', function () {
+        expect(sayHello("World")).toBe("Hello, World!")
+    });
+    it('should sayHello(true) return "Hello, World!', function () {
+        expect(sayHello(true)).toBe("Hello, World!")
+    });
+    it('should sayHello(false) return "Hello, World!', function () {
+        expect(sayHello(false)).toBe("Hello, World!")
+    });
 })
